56a3e1c732fb44ec2ee9093aa03472fca69f94df
clarelei <clarelei@yahoo.com>
2017-07-26 21:00:45 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# markerville
+# markerville-master
---
+++ public/js/database.js
@@ -0,0 +1,24 @@
+class Database {
+  constructor(containerElement){
+    this.containerElement = containerElement;
+    this.prevTable = document.querySelector("#result table tbody");
+    this.message = document.querySelector('#result p');
+    this.clearDatabase = this.clearDatabase.bind(this);
+  }
+
+  show(){
+    this.containerElement.classList.remove('inactive');
+    this.clearDatabase();
+
+  }
+
+  hide(){
+    this.containerElement.classList.add('inactive');
+    this.clearDatabase();
+  }
+
+  clearDatabase(){
+    this.prevTable.innerHTML = "";
+  }
+
+}
