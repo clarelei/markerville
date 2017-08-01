b14c9f8e26ae043eca724098d065b25b4c5dd8cc
clarelei <clarelei@yahoo.com>
2017-08-01 11:02:26 -0700

    Add files via upload

--- public/js/app.js
+++ public/js/app.js
@@ -12,8 +12,6 @@ class App {
     const result = document.querySelector('#result');
     this.result = new Result(result);
 
-    const resourcesPage = document.querySelector('#resourcesPage');
-    this.resourcesPage = new Resources(resourcesPage);
 
     this.forumInput = document.querySelector('#forum form');
 
@@ -23,19 +21,16 @@ class App {
     this.showForum = this.showForum.bind(this);
     this.showAbout = this.showAbout.bind(this);
     this.showResult = this.showResult.bind(this);
-    this.showResources = this.showResources.bind(this);
 
     this.databaseButton = document.querySelector('#databaseButton');
     this.forumButton = document.querySelector('#forumButton');
     this.aboutButton = document.querySelector('#aboutButton');
     this.submitButton = document.querySelector("#searchForm");
-    this.resourcesButton = document.querySelector('#resourcesButton')
 
     this.databaseButton.addEventListener('click', this.showDatabase);
     this.forumButton.addEventListener('click', this.showForum);
     this.aboutButton.addEventListener('click', this.showAbout);
     this.submitButton.addEventListener('submit', this.showResult);
-    this.resourcesButton.addEventListener('click', this.showResources);
 
 
 
@@ -47,7 +42,6 @@ class App {
     this.database.show();
     this.aboutPage.hide();
     this.result.hide();
-    this.resourcesPage.hide();
 
   }
 
@@ -56,7 +50,6 @@ class App {
     this.forum.show();
     this.aboutPage.hide();
     this.result.hide();
-    this.resourcesPage.hide();
 
   }
 
@@ -65,7 +58,6 @@ class App {
     this.forum.hide();
     this.aboutPage.show();
     this.result.hide();
-    this.resourcesPage.hide();
   }
 
   showResult(){
@@ -73,16 +65,6 @@ class App {
     this.forum.hide();
     this.aboutPage.hide();
     this.result.show();
-    this.resourcesPage.hide();
-
-  }
-
-  showResources(){
-    this.database.hide();
-    this.forum.hide();
-    this.aboutPage.hide();
-    this.result.hide();
-    this.resourcesPage.show();
 
   }
 
