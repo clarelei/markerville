a15823bb1f06d357cb57b3b1e0c64fab24697576
clarelei <clarelei@yahoo.com>
2017-08-02 14:09:39 -0700

    Update fetchData.js

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -1,3 +1,44 @@
+// //make sure is called
+// async function onSearchData(event) {
+ 
+//   event.preventDefault();
+  
+//   const input = document.querySelector('#word-input');
+//   const word = input.value.trim();
+//   const results = document.querySelector('#database');
+//   const result = await fetch('/lookup/' + word);
+//   const json = await result.json();
+//   const resultWord = json.word;
+//   const associated = json.associated;
+//   var count = 0;
+//   if(associated.length != 0 && word.length!=0){
+
+//   for (let obj in associated){
+//     count++;
+//     var rowToAdd = ["","","","",""];
+//     rowToAdd[0] = associated[obj].markerName;
+
+//     rowToAdd[1] = associated[obj].biomarkerType;
+
+    
+//     rowToAdd[2] = associated[obj].diseaseType;
+
+//     rowToAdd[3] = associated[obj].associatedDrug;
+
+//     rowToAdd[4] = associated[obj].medium;
+
+//     table.row.add(rowToAdd).draw();
+//     }
+//     console.log(count);
+
+// }
+// } 
+// var table = $('#testid').DataTable();
+// const searchForm = document.querySelector('#searchForm');
+// searchForm.addEventListener('submit', onSearchData);
+
+
+
 //make sure is called
 async function onSearchData(event) {
  
@@ -6,7 +47,10 @@ async function onSearchData(event) {
   const input = document.querySelector('#word-input');
   const word = input.value.trim();
   const results = document.querySelector('#database');
-  const result = await fetch('/lookup/' + word);
+  const result = ""; 
+  if (word in results) {
+   result = await fetch('/lookup/' + word);
+  }
   const json = await result.json();
   const resultWord = json.word;
   const associated = json.associated;
@@ -36,10 +80,6 @@ async function onSearchData(event) {
 var table = $('#testid').DataTable();
 const searchForm = document.querySelector('#searchForm');
 searchForm.addEventListener('submit', onSearchData);
-
-
-
-
 
 
 //   const re = new RegExp(word, "gi");
